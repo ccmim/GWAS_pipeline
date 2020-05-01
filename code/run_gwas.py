cc95c9e741614eaaafa15fdf776fd758cb3bbae6
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2020-05-01 13:40:06 +0100

    minor changes in config file reading

--- code/run_gwas.py
+++ code/run_gwas.py
@@ -23,16 +23,23 @@ class GWAS_Run:
         # else:
         #     self.phenotypes = config.get("phenotypes_list").strip().split()
 
+        # file with phenotype values
+        ## file name patterns
+        fp_config = config["filename_patterns"]
+        self.pheno_f = os.path.join(config["data_dir"], fp_config["phenotype"]["phenotype_file"])
+        # temporal file with phenotype values
+        self.pheno_f_tmp = os.path.join(config["data_dir"], fp_config["phenotype"]["phenotype_file_tmp"])
+
         phenotype_list = config["phenotype_list"]
-        if os.path.exists(os.path.join(os.path.dirname(self.yaml_config_file), phenotype_list)):
+        if phenotype_list is None:
+            self.phenotypes = open(self.pheno_f).readline().strip().split(",")
+            self.phenotypes.remove("IID")
+        elif os.path.exists(os.path.join(os.path.dirname(self.yaml_config_file), phenotype_list)):
             phenotype_list = os.path.join(os.path.dirname(self.yaml_config_file), phenotype_list)
             self.phenotypes_all = [x.strip() for x in open(phenotype_list)]
             self.phenotypes = self.phenotypes_all
-        else:
+        elif isinstance(phenotype_list, list):
             self.phenotypes = phenotype_list
-
-        ## file name patterns
-        fp_config = config["filename_patterns"]
 
         # self.indiv_f = fp_config.get("individuals", None)
 
@@ -40,21 +47,20 @@ class GWAS_Run:
         self.bim_fp = fp_config["genotype"]["bim"].format(data_dir=config["data_dir"])
         self.fam_fp = fp_config["genotype"]["fam"].format(data_dir=config["data_dir"])
 
-        # file with phenotype values
-        self.pheno_f = os.path.join(config["data_dir"], fp_config["phenotype"]["phenotype_file"])
-        # temporal file with phenotype values
-        self.pheno_f_tmp = os.path.join(config["data_dir"], fp_config["phenotype"]["phenotype_file_tmp"])
-
         self.gwas_fp = os.path.join(config["output_dir"], fp_config["gwas"])
+        self.gwas_fp = self.gwas_fp.format(**(config["tokens"]))
 
-        options = config["options"]
-        self.filename_rules = config["suffix_tokens"]
+        # self.filename_rules = config["suffix_tokens"]
 
-        options_suffix_tokens = {}
-        for k in options.keys():
-            options_suffix_tokens[k] = self.filename_rules[k][options[k]]
+        # options = config["options"]
+        # options_suffix_tokens = {}
 
-        self.output_suffix = config["suffix"].format(**options_suffix_tokens)
+        # if options is not None:
+        #   for k in options.keys():
+        #     options_suffix_tokens[k] = self.filename_rules[k][options[k]]
+        #   self.output_suffix = config["suffix"].format(**options_suffix_tokens)
+        # else:
+        #   self.output_suffix = config["suffix"]
 
         ## covariates list
         self.covariates_f = fp_config.get("covariates", None)
@@ -106,8 +112,8 @@ class GWAS_Run:
         IID|FID|phenotype1|phenotype2|...
         '''
 
-        df = pd.read_table(self.pheno_f)
-        df.rename(columns={"Subject ID": "IID"}, inplace=True)
+        df = pd.read_table(self.pheno_f, sep=',')
+        # df.rename(columns={"Subject ID": "IID"}, inplace=True)
 
         df["FID"] = df["IID"]  # duplicate ID column in FID column
         # print(df.head())
@@ -171,7 +177,7 @@ class GWAS_Run:
     def execute(self, chromosome, phenotype):
         # TODO: add log messages (redirect error output?)
         print("  Processing chromosome {}...".format(chromosome))
-        output_basename = self.gwas_fp.format(phenotype=phenotype, suffix="%s__chr%s" % (self.output_suffix, chromosome))
+        # output_basename = self.gwas_fp.format(phenotype=phenotype, suffix="%s__chr%s" % (self.output_suffix, chromosome))
         # print("    Generating file {}.qassoc...".format(output_basename))
         command = [self.plink_exec,
                    "--bed", self.bed_fp.format(chromosome=chromosome),
@@ -179,7 +185,8 @@ class GWAS_Run:
                    "--fam", self.fam_fp.format(chromosome=chromosome),
                    "--pheno", self.pheno_f_tmp,
                    "--pheno-name", phenotype,
-                   "--out", output_basename]
+                   "--out", self.output_file]
+                   # "--out", output_basename]
 
         if self.covariates_f is None:
             command += ["--assoc"]
@@ -210,20 +217,23 @@ class GWAS_Run:
 
         for phenotype in self.phenotypes:
             print("Processing {}...".format(phenotype))
-            output_filename = self.gwas_fp.format(phenotype=phenotype, suffix=self.output_suffix)
-            os.makedirs(os.path.dirname(output_filename), exist_ok=True)
-            if os.path.exists(output_filename) and not self.overwrite_output:
-                print("Output file already exists, if a new run is desired please delete the previous file.")
-                continue
-            else:
-                for chr in sorted(int(i) for i in self.chromosomes):
+            self.gwas_fp_ = self.gwas_fp + "__chr{chr}"
+            os.makedirs(os.path.dirname(self.gwas_fp_), exist_ok=True)
+            for chr in sorted(int(i) for i in self.chromosomes): # cast to int to be able to order the chromosomes
+                self.output_file = self.gwas_fp_.format(phenotype=phenotype, chr=chr)
+                if os.path.exists(self.output_file) and not self.overwrite_output:
+                    print("Output file already exists, if a new run is desired please delete the previous file.")
+                    continue
+                else:
                     self.execute(str(chr), phenotype)
 
             if self.merge_chromosomes:
 
-                with open(output_filename, "w") as merged_fh:
+                self.output_file = self.gwas_fp.format(phenotype=phenotype)
+                with open(self.output_file + "csv", "w") as merged_fh:
                     ## TODO: suffix "qassoc" is added ad hoc, it would be better to include as attribute in the class
-                    partitioned_gwas_files = [self.gwas_fp.format(phenotype=phenotype, suffix="%s__chr%s.qassoc" % (self.output_suffix, chr)) for chr in self.chromosomes]
+                    # self.gwas_fp_ = self.gwas_fp + "__chr{chr}"
+                    partitioned_gwas_files = [self.gwas_fp_.format(phenotype=phenotype, chr=chr) + ".qassoc" for chr in self.chromosomes]
                     for i, file_chr in enumerate(partitioned_gwas_files):
                         try:
                             with open(file_chr) as chr_fh:
@@ -232,7 +242,7 @@ class GWAS_Run:
                                         merged_fh.write("%s\n" % "\t".join(line.strip().split()))
                                 os.remove(file_chr)
                         except:
-                           from IPython import embed; embed()
+                           pass # from IPython import embed; embed()
 
         if self.delete_temp:
             os.remove(self.pheno_f_tmp)
