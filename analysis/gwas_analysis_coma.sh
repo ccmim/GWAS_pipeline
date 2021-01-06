89e28204815eb17c466e7b773e3c28a8da6aad6c
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2021-01-06 02:03:52 +0000

    Code cleanup

--- analysis/gwas_analysis_coma.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd $(git rev-parse --show-toplevel)
-
-for GWAS_FOLDER in `ls output/coma`; do
-  Rscript analysis/gwas_analysis_new.R --gwas_folder $GWAS_FOLDER --cache_rds --qqplot_pooled
-done
