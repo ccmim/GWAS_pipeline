372a8ac3dbb0701a590f78cfa8015e1605ac0852
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2021-01-06 01:53:49 +0000

    Fixed script

--- src/adjust_for_covariates.R
+++ src/adjust_for_covariates.R
@@ -8,93 +8,128 @@ library(tidyverse)
 library(stringr)
 
 parser <- ArgumentParser()
-parser$add_argument("-i", "--input_file")
-parser$add_argument("-o", "--output_file")
+parser$add_argument("-i", "--input_file", required=TRUE)
+parser$add_argument("-o", "--output_file", required=TRUE)
 parser$add_argument("-c", "--covariates_file", default="data/covariates.csv")
-parser$add_argument("--covariates")
+parser$add_argument("--covariates", nargs="+", default=c("X50", "X4079", "X4080", "X21001", "X21003", "X31"))
+parser$add_argument("--impute_with_mean_for", nargs="+", default=c("X4079", "X4080"))
 parser$add_argument("--white_list", default=NULL)
 parser$add_argument("--phenotypes", default=NULL, nargs="+")
 parser$add_argument("--phenotypes_black_list", default=NULL, nargs="+")
-parser$add_argument("--keep_non_inv_norm", action="store_true", default=FALSE)
+parser$add_argument("--keep_non_cov_adj", action="store_true", default=FALSE, help="If set, keep non-covariate adjusted phenotypes")
 args <- parser$parse_args()
 
-field_codes <- c(
-  "gender"="X31",
-  "height"="X50",
-  "BMI"="X21001",
-  "weight"="X21002",
-  "genetic_gender"="X22001",
-  "ethnicity"="X21000",
-  "age_at_recruitment"="X21003",
-  "DBP"="X4079", #diastolic blood pressure
-  "SBP"="X4080" #systolic blood pressure
-)
+## AUXILIARY FUNCTIONS
 
-if (!file.exists(args$input_file)) {
-  quit(status = 1)
+impute_na <- function(df, columns, method="mean") {
+  for (column in columns) {
+    if (method == "mean") {
+      df[is.na(df[, column]), column] <- mean(df[, column], na.rm = TRUE)
+    }
+  }
+  df
 }
 
-covariates_df <- read.csv(args$covariates_file)
+mean_across_visits <- function(df, columns, id_column="eid") {
+  for (column in columns) {
+    # new_colname <- glue::glue("X{ufc}")
+    new_col <- df %>% select(starts_with(column)) %>% rowMeans(na.rm=TRUE)
+    df[column] <- new_col
+  }
+  
+  # remove columns for each individual visit to the assessment centre
+  df <- df %>% select(id_column, columns)
+ 
+  df
+}
 
-pheno_df <- read.table(args$input_file, sep = ",", header=TRUE) %>% select(-subset) # subset has the partition (train, test) to which subject belongs. We don't need that now.
+get_adjusted_phenotype <- function(df, phenotype, covariates) {
+  # Generate formula: phenotype ~ covariate_1 + covariate_2 + ...
+  formula_as_text <- "{phenotype} ~ {paste(covariates, collapse = \" + \")}"
+  fit <- lm(formula=as.formula(glue::glue(formula_as_text)), data=df)
+  resid(fit)
+}
 
-if (!is.null(args$phenotypes)) {
-  pheno_names <- args$phenotypes
+# Rank inverse-normalization
+inverse_normalise <- function(x) 
+  qnorm( (rank(x,na.last="keep")-0.5) / sum(!is.na(x)) )
+
+## END AUXILIARY FUNCTIONS
+
+## Load phenotype data
+if (!file.exists(args$input_file)) {
+  quit(status = 1)
 } else {
+  pheno_df <- read.table(args$input_file, sep = ",", header=TRUE)
+}
+
+## Load covariates data
+covariates_df <- read.csv(args$covariates_file)
+covariates <- args$covariates
+
+covariates_df <- mean_across_visits(covariates_df, covariates)
+
+# impute columns with the population mean when missing
+covariates_df <- impute_na(covariates_df, args$impute_with_mean_for)
+  
+# remove rows with missing data
+covariates_df <- na.omit(covariates_df)
+  
+if (is.null(args$phenotypes)) {
   pheno_names <- colnames(pheno_df)
   pheno_names <- pheno_names[pheno_names != "ID"]
   print(pheno_names)
+} else {
+  pheno_names <- args$phenotypes
 }
 
 if (!is.null(args$phenotypes_black_list)) {
   pheno_names <- pheno_names[!pheno_names %in% args$phenotypes_black_list]
 }
 
+# Add covariates to the phenotype file to then perform linear regression
+# ID | phenotype_1 | phenotype_2 | ... | covariate_1 | covariate_2 | ...
 pheno_df <- pheno_df %>% left_join(covariates_df, by = c("ID"="eid"))
 
-# Remove X prefix
-unique_field_codes <- unlist(sapply(colnames(pheno_df), strsplit, "\\.")) %>%
-  .[grepl(pattern = "X", x = .)] %>%
-  gsub(pattern = "X", replacement = "") %>% unique
+## Obtain covariate-adjusted phenotypes
+new_colnames <- paste0(pheno_names, "_adj")
 
-for (ufc in unique_field_codes) {
-  new_colname <- glue::glue("X{ufc}")
-  new_col <- pheno_df %>% select(starts_with(glue::glue("X{ufc}."))) %>% rowMeans(na.rm=TRUE)
-  pheno_df[new_colname] <- new_col
-}
-
-pheno_df <- pheno_df %>% select(-matches(".\\..\\."))
-pheno_df[is.na(pheno_df[, "X4079"]), "X4079"] <- mean(pheno_df[, "X4079"], na.rm = TRUE)
-pheno_df[is.na(pheno_df[, "X4080"]), "X4080"] <- mean(pheno_df[, "X4080"], na.rm = TRUE)
-pheno_df <- na.omit(pheno_df)
-
-# Obtain covariate-adjusted phenotypes
-for (pheno in pheno_names) {
-  new_colname <- paste0(pheno, "_adj")
-  pheno_df[, new_colname] <- NA
-  fit <- lm(formula=pheno_df[, pheno] ~ pheno_df$X50 + pheno_df$X4079 + pheno_df$X21001 + pheno_df$X21003 + pheno_df$X31)
-  pheno_df[, new_colname] <- resid(fit)
+for (i in seq_along(pheno_names)) {
+  pheno_df[, new_colnames[i]] <- NA
+  pheno_df[, new_colnames[i]] <- get_adjusted_phenotype(pheno_df, pheno_names[i], covariates)
 }
 
 # Reorder columns
-pheno_df <- cbind(pheno_df %>% select(-starts_with("X")), pheno_df %>% select(starts_with("X")))
+# ID | phenotype_1 | phenotype_2 | ... | covariate_1 | covariate_2 | ... | covariate_1_adj | covariate_2_adj | ...
+pheno_df <- cbind(pheno_df %>% select(-covariates), pheno_df %>% select(covariates))
 
-# Retrieve adjusted phenotypes
-adj_phenos <- colnames(pheno_df)[grepl(pattern = "adj", colnames(pheno_df))]
+# Inverse-normalise
+pheno_df_ <- pheno_df
+pheno_df <- pheno_df_ %>% select(ID) %>% rename(IID=ID)
 
-# Rank inverse-normalization
-kk <- apply(
-  pheno_df %>% select(-ID, -starts_with("X")),
+# Keep non-covariate adjusted phenotypes
+if (args$keep_non_cov_adj) {
+  inv_norm_non_cov_adj_df <- apply(
+    pheno_df_ %>% select(pheno_names),
+    MARGIN = 2,
+    FUN = inverse_normalise
+  )
+  pheno_df <- cbind(pheno_df, as.data.frame(inv_norm_non_cov_adj_df))
+  rm(inv_norm_non_cov_adj_df)
+}
+
+inv_norm_cov_adj_df <- apply(
+  pheno_df_ %>% select(new_colnames),
   MARGIN = 2,
-  FUN = function(x) qnorm((rank(x,na.last="keep")-0.5)/sum(!is.na(x)))
+  FUN = inverse_normalise
 )
 
-pheno_df <- cbind(pheno_df %>% select(ID) %>% rename(IID=ID), as.data.frame(kk))
-rm(kk)
+pheno_df <- cbind(pheno_df, as.data.frame(inv_norm_cov_adj_df))
+rm(inv_norm_cov_adj_df)
 
-pheno_df <- cbind(pheno_df["IID"], pheno_df %>% select(ends_with("adj")))
-print(head(pheno_df))
-
-
-filename <- args$output_file
-write_delim(x = pheno_df, filename, col_names = TRUE, delim = ",", na = "NA")
+# Write output into file
+write_delim(
+  x = pheno_df,
+  args$output_file, 
+  col_names = TRUE, delim = ",", na = "NA"
+)
