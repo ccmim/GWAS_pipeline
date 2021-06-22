e40540bcefc510de526bb278ab360d3fa183f144
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2021-06-22 13:31:04 +0100

    Added test bash script

---
+++ tests/gwas_coma_PLINK_adj_for_PCs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Move to the root directory of this repository
+cd $(git rev-parse --show-toplevel)
+
+EXPERIMENT="2020-09-11_02-13-41"
+
+python main.py \
+  --yaml_config_file config_files/config_coma.yaml \
+  --coma_experiment ${EXPERIMENT} \
+  --gwas_file tests/examples/{experiment}/{suffix}/GWAS__{{phenotype}}__{suffix} \
+  --suffix "test_{covariates_config}__{sample_white_lists}__{quality_control}" \
+  --gwas_software plink \
+  --chromosomes 6  
