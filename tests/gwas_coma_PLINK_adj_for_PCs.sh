9f5bfc3eaf12533b1f2c2e64ea4181d0b19a7ba6
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2021-06-27 10:47:53 +0100

    Added configuration for LVEDV-adjusted GWAS

--- tests/gwas_coma_PLINK_adj_for_PCs.sh
+++ tests/gwas_coma_PLINK_adj_for_PCs.sh
@@ -7,6 +7,7 @@ EXPERIMENT="2020-09-11_02-13-41"
 
 python main.py \
   --yaml_config_file config_files/config_coma.yaml \
+  --covariates config_files/covariates/std_covariates_10PCs_and_LVEDV.yaml \
   --coma_experiment ${EXPERIMENT} \
   --gwas_file tests/examples/{experiment}/{suffix}/GWAS__{{phenotype}}__{suffix} \
   --suffix "test_{covariates_config}__{sample_white_lists}__{quality_control}" \
